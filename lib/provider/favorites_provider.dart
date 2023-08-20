643ec84a9659a7f4f5f7874a28dd21259f5dfa1d
CHETHANAR11 <chethanasree.r2021it@sece.ac.in>
2023-08-20 21:54:50 +0530

    fifth commit

---
+++ lib/provider/favorites_provider.dart
@@ -0,0 +1,24 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:meals/models/meal.dart';
+
+class FavoriteMealsNotifier extends StateNotifier<List<Meal>> {
+  FavoriteMealsNotifier() : super([]);
+
+  bool toggleMealFavoritestatus(Meal meal) {
+    final mealsIsFavorite = state.contains(meal);
+
+    if (mealsIsFavorite) {
+      state = state.where((m) => m.id != meal.id).toList();
+      return false; //removed
+    } else {
+      state = [...state, meal];
+      return true; //add
+    }
+  }
+}
+
+final favoriteMealsProvider =
+    StateNotifierProvider<FavoriteMealsNotifier, List<Meal>>((ref) {
+  return FavoriteMealsNotifier();
+});
